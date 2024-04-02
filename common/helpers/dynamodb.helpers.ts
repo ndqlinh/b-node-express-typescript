273a46f3e94f0fe30987f57a6aa5ab3f7de0f82a
ndqlinh <ndqlinh36@gmail.com>
2024-04-02 12:02:15 +0700

    create dynamodb helper

---
+++ common/helpers/dynamodb.helpers.ts
@@ -0,0 +1,28 @@
+import { appConfig } from '@config/index';
+import {
+  BillingMode,
+  GlobalSecondaryIndex,
+  RemovalPolicy,
+  TableDefinition,
+  TableKey
+} from '../types/dynamodb.type';
+
+export class BaseTableDefinition implements TableDefinition {
+  name: string;
+  partitionKey: TableKey;
+  sortKey?: TableKey;
+  pointInTimeRecovery?: boolean;
+  billingMode?: BillingMode;
+  removalPolicy?: RemovalPolicy;
+  indexes?: GlobalSecondaryIndex[] = [];
+
+  constructor(data: TableDefinition) {
+    this.name = `NodeExpress${data.name}Table`;
+    this.partitionKey = data.partitionKey;
+    this.sortKey = data.sortKey;
+    this.pointInTimeRecovery = data.pointInTimeRecovery;
+    this.billingMode = data.billingMode || BillingMode.PAY_PER_REQUEST;
+    this.removalPolicy = data.removalPolicy || RemovalPolicy.RETAIN;
+    this.indexes = data.indexes;
+  }
+}
