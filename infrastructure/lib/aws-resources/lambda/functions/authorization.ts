c6d797c399e000b4d579f3d226893808dafa15ae
ndqlinh <ndqlinh36@gmail.com>
2024-04-03 10:59:41 +0700

    add dynamodb permission for authorization lambda function

--- infrastructure/lib/aws-resources/lambda/functions/authorization.ts
+++ infrastructure/lib/aws-resources/lambda/functions/authorization.ts
@@ -1,7 +1,12 @@
+import { DynamodbPermission } from '@common/types/dynamodb.type';
 import { LambdaFunction } from '../lambda.helper';
+import { ACCOUNTS_TABLE_NAME } from '@db/dynamodb/tables';
 
 export const authorizationFunction = new LambdaFunction({
   functionName: 'ApiAuthorizer',
   entry: 'authorizer.controller.ts',
-  ssm: 'ApiAuthorizer'
+  ssm: 'ApiAuthorizer',
+  dynamodbTables: {
+    [ACCOUNTS_TABLE_NAME]: [DynamodbPermission.FULL, DynamodbPermission.INDEX]
+  }
 });
