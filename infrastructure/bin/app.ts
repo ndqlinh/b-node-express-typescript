8770125c6bbac36bf37fbdbbacc92612d42f8167
ndqlinh <ndqlinh36@gmail.com>
2024-04-03 08:25:21 +0700

    finished define stacks

---
+++ infrastructure/bin/app.ts
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
+import { ApigatewayStack, BaseStack, DynamodbStack, LambdaStack } from '../lib';
+import { appConfig } from '@config/index';
+
+const app = new cdk.App();
+// Note: Stacks will be executed in A-Z order of stack id
+new DynamodbStack(app, `NodeExpress-A-DynamodbStack`, {
+  env: {
+    account: appConfig.profile.accountId,
+    region: appConfig.profile.region
+  }
+});
+
+new BaseStack(app, `NodeExpress-B-BaseStack`, {
+  env: {
+    account: appConfig.profile.accountId,
+    region: appConfig.profile.region
+  }
+});
+
+new LambdaStack(app, `NodeExpress-C-LambdaStack`, {
+  env: {
+    account: appConfig.profile.accountId,
+    region: appConfig.profile.region
+  }
+});
+
+new ApigatewayStack(app, `NodeExpress-D-ApigatewayStack`, {
+  env: {
+    account: appConfig.profile.accountId,
+    region: appConfig.profile.region
+  }
+});
---
+++ infrastructure/bin/development.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+import { appConfig } from '@config/index';
+import * as cdk from 'aws-cdk-lib';
+import { RestApi, TokenAuthorizer } from 'aws-cdk-lib/aws-apigateway';
+import { Construct } from 'constructs';
+import 'source-map-support/register';
+
+import { ApigatewayConstruct } from '../lib/aws-resources/api-gateway/api-gateway.construct';
+import { apiResources } from '../lib/aws-resources/api-gateway/resources';
+import { lambdaFunctions } from '../lib/aws-resources/lambda';
+import { LambdaConstruct } from '../lib/aws-resources/lambda/lambda.construct';
+
+class DevelopmentStack extends cdk.Stack {
+  restApi: RestApi;
+  authorizer: TokenAuthorizer;
+
+  constructor(scope: Construct, id: string, props: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create API Gateway
+    const apigatewayConstruct = new ApigatewayConstruct(
+      this,
+      `NodeExpressApigatewayConstruct`
+    );
+    apigatewayConstruct.createApiGateway(this);
+
+    // Create Lambda Functions
+    const lambdaConstruct = new LambdaConstruct(this, `NodeExpressLambdaConstruct`);
+    const funcs = lambdaConstruct.createLambdaFunctions(this, [...lambdaFunctions]);
+
+    // Create API Resources
+    apigatewayConstruct.createApiAuthorizer(this);
+    apigatewayConstruct.createApiResources(this, apiResources, funcs);
+  }
+}
+
+const app = new cdk.App();
+// Note: Stacks will be executed in A-Z order of stack id
+new DevelopmentStack(app, 'DevelopmentStack', {
+  env: {
+    account: appConfig.profile.accountId,
+    region: appConfig.profile.region
+  }
+});
