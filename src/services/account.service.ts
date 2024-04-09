0afcdaa204863a156608e4439b9016f63a6b3929
ndqlinh <ndqlinh36@gmail.com>
2024-04-09 14:59:36 +0700

    create account service

---
+++ src/services/account.service.ts
@@ -0,0 +1,13 @@
+import { Account } from "../models/account.model";
+
+export default class AccountService {
+  constructor() {}
+
+  register(account: any): Promise<Account> {
+    return;
+  }
+
+  login(account: any): Promise<Account> {
+    return;
+  }
+}
