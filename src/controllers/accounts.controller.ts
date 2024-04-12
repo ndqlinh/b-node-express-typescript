f1a0187e825e8c7de36834514d431a299c11a23f
ndqlinh <ndqlinh36@gmail.com>
2024-04-12 14:58:21 +0700

    using res.send instead of base response

--- src/controllers/accounts.controller.ts
+++ src/controllers/accounts.controller.ts
@@ -2,9 +2,7 @@ import express, { Request, Response } from 'express';
 import serverless from 'serverless-http';
 import { ROUTES } from '@config/routes';
 import AccountService from '../services/account.service';
-import { HttpException } from '@shared/helpers/exception.helper';
-import { HTTPStatus } from '@shared/enums/http.enum';
-import { BaseResponse } from '@shared/helpers/response.helper';
+import { StatusCodes } from '@common/enums/status-codes.enum';
 
 const app = express();
 
@@ -15,14 +13,14 @@ app.post(ROUTES.register, async (req: Request, res: Response, next) => {
   const userInfo = req.body;
   const account = new AccountService();
   const registeredAcount = await account.register(userInfo);
-  return BaseResponse.toSuccess(registeredAcount);
+  return res.send({ code: StatusCodes.Ok, data: registeredAcount });
 });
 
 app.post(ROUTES.signin, async (req: Request, res: Response, next) => {
   const account = new AccountService();
   const verifiedAccount = await account.verify(req.body);
   const result = await account.login(verifiedAccount);
-  return BaseResponse.toSuccess(result);
+  return res.send({ code: StatusCodes.Ok, data: result });
 });
 
 export const handler = serverless(app);
