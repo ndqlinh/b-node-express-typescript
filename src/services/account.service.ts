48d2c2c3076432fed421e1008cf7796e4c2dc144
ndqlinh <ndqlinh36@gmail.com>
2024-04-12 14:04:28 +0700

    remove unused code

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -53,22 +53,18 @@ export default class AccountService {
   }
 
   async verify(account: any): Promise<Account> {
-    try {
-      const tartgetAccount: Account = await this.accountRepository.findByEmail(account.email);
+    const tartgetAccount: Account = await this.accountRepository.findByEmail(account.email);
 
-      if (!tartgetAccount) {
-        throw new HttpException(HTTPStatus.NOT_FOUND, 'Account does not exist');
+    if (!tartgetAccount) {
+      throw new HttpException(HTTPStatus.NOT_FOUND, 'Account does not exist');
+    } else {
+      const hashedPassword = await this.hashPassword(account.password);
+      const isMatchedPassword = hashedPassword === tartgetAccount.password;
+      if (isMatchedPassword) {
+        return tartgetAccount;
       } else {
-        const hashedPassword = await this.hashPassword(account.password);
-        const isMatchedPassword = hashedPassword === tartgetAccount.password;
-        if (isMatchedPassword) {
-          return tartgetAccount;
-        } else {
-          throw new HttpException(HTTPStatus.BAD_REQUEST, 'Invalid email or password');
-        }
+        throw new HttpException(HTTPStatus.BAD_REQUEST, 'Invalid email or password');
       }
-    } catch (error) {
-      throw new HttpException(HTTPStatus.INTERNAL_SERVER_ERROR, 'Verify account failed', error);
     }
   }
 }
