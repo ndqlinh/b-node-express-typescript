020d97d237a92aad6e7552a574fd80f20296705c
st-linhnguyen <linh.nguyen@supremetech.vn>
2024-04-23 20:27:58 +0700

    remove unused code

--- src/shared/handler.ts
+++ src/shared/handler.ts
@@ -1,5 +1,3 @@
-import { appConfig } from '@config/index';
-import AuthService from '../services/auth.service';
 import { Logger } from './helpers/logger.helper';
 import { BaseResponse } from './helpers/response.helper';
 
@@ -9,19 +7,6 @@ export const wrapper =
     try {
       if (event.body) {
         event.body = typeof event.body == 'object' ? event.body : JSON.parse(event.body);
-      }
-
-      const authHeader = event?.authorizationToken;
-      const token = authHeader && authHeader.split(' ')[1];
-
-      if (appConfig.env === 'local' && token) {
-        const auth = new AuthService();
-        const verifyResult: any = await auth.verifyToken(token);
-        Logger.INFO('verifyResult', verifyResult);
-
-        if (verifyResult.email) {
-          event.user = verifyResult;
-        }
       }
 
       const result = await handler({ ...event } as any, context, callback);
