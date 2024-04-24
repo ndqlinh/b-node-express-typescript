a136f0fd457849d60007569cd22b988e9c3e18c5
ndqlinh <ndqlinh36@gmail.com>
2024-04-24 16:10:07 +0700

    modify authorizer controller

--- src/controllers/authorizer.controller.ts
+++ src/controllers/authorizer.controller.ts
@@ -2,8 +2,9 @@ import { Logger } from '@shared/helpers/logger.helper';
 import { wrapper } from '../shared/handler';
 import { generatePolicy } from '../shared/utils/policy.util';
 import AuthService from '../services/auth.service';
+import { Context } from 'aws-lambda';
 
-export const handler = wrapper(async (event: any, context: any, callback): Promise<any> => {
+export const handler = wrapper(async (event: any, context: Context, callback): Promise<any> => {
   const auth = new AuthService();
   const { authorizationToken, methodArn } = event;
 
@@ -11,14 +12,13 @@ export const handler = wrapper(async (event: any, context: any, callback): Promi
     const token = authorizationToken.replace('Bearer', '').trim();
     const verificationResult: any = await auth.verifyToken(token);
     Logger.INFO('VERIFICATION RESULT', verificationResult);
-
-    const policy = generatePolicy('Allow', methodArn, {
+    const policy = generatePolicy('Allow', methodArn);
+    callback(null, policy, {
       ownerId: verificationResult.id,
       email: verificationResult.email
     });
-    context.succeed(policy);
   } catch (err: any) {
     Logger.INFO('AUTHORIZE ERROR', err);
-    context.fail('Unauthorized');
+    callback('Unauthorized');
   }
 });
