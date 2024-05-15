39ecbd96fdb26c15ca5811f52910e3f036af2768
ndqlinh <ndqlinh36@gmail.com>
2024-05-15 16:08:12 +0700

    modify get profile detail

--- infrastructure/lib/aws-resources/lambda/functions/profile.ts
+++ infrastructure/lib/aws-resources/lambda/functions/profile.ts
@@ -2,13 +2,25 @@ import { LambdaFunction } from '../lambda.helper';
 import { ROUTES } from '@config/routes';
 import { DynamodbPermission } from '@common/types/dynamodb.type';
 import { ACCOUNTS_TABLE_NAME } from '@db/dynamodb/tables';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 const lambdaOpions = {
   entry: 'profile.controller.ts',
   auth: true,
   dynamodbTables: {
     [ACCOUNTS_TABLE_NAME]: [DynamodbPermission.FULL, DynamodbPermission.INDEX]
-  }
+  },
+  customPolicies: [
+    new PolicyStatement({
+      actions: [
+        'ssm:GetParameterHistory',
+        'ssm:GetParametersByPath',
+        'ssm:GetParameters',
+        'ssm:GetParameter'
+      ],
+      resources: ['*']
+    })
+  ]
 };
 
 const profileDetailFunction = new LambdaFunction({
--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,12 +1,17 @@
 import { wrapper } from '@shared/handler';
 import { BaseResponse } from '@shared/helpers/response.helper';
 import ProfileService from '../services/profile.service';
+import AuthService from '../services/auth.service';
 
 const user = new ProfileService();
+const auth = new AuthService();
 
 export const getProfileDetail = wrapper(async (event: any, _context: any, callback) => {
-  const requestBody = event.body;
-  const profile = await user.findByEmail(requestBody?.email);
+  const authorizationHeader = event.headers['Authorization'] || event.headers['authorization'];
+  const token = authorizationHeader.split(' ')[1];
+  const decodedToken: any = await auth.verifyToken(token);
+  const { email } = decodedToken;
+  const profile = await user.findByEmail(email);
 
   return BaseResponse.toSuccess(profile);
 });
