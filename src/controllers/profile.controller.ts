a37fbd4fc843217589bdc3869550c181d1ec093e
st-linhnguyen <linh.nguyen@supremetech.vn>
2024-05-31 14:07:32 +0700

    update code logic to get profile detail

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -6,10 +6,7 @@ const user = new ProfileService();
 const auth = new AuthService();
 
 export const getProfileDetail = async (event: any, _context: any, callback) => {
-  const authorizationHeader = event.headers['Authorization'] || event.headers['authorization'];
-  const token = authorizationHeader.split(' ')[1];
-  const decodedToken: any = await auth.verifyToken(token);
-  const { email } = decodedToken;
+  const { email } = event;
   const profile = await user.findByEmail(email);
 
   return BaseResponse.toSuccess(profile);
